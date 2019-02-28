27217f2c401f648c1ac69ad89e80677b7b6e707c
weiqi <wiki0831@gmail.com>
2019-02-28 18:48:31 -0500

    circular push

--- Assets/Scripts/Circular.cs
+++ Assets/Scripts/Circular.cs
@@ -3,19 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Circular : MonoBehaviour {
-    float timeCounter = 0;
-    public float speed = 1;
-    public int radius = 10;
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        timeCounter += Time.deltaTime * speed;
-        float x = Mathf.Sin(timeCounter * radius);
-        float y = Mathf.Cos(timeCounter * radius);
-        transform.localPosition = new Vector3(x, 0, y);
-	}
+    public Transform center;
+    public Vector3 axis = Vector3.up;
+    public float radius = 2.0F;
+    public float radiusSpeed = 0.5F;
+    public float rotationSpeed = 80.0F;
+
+    void Start()
+    {
+        center = gameObject.transform;
+        transform.position = (transform.position - center.position).normalized * radius + center.position;
+    }
+
+    void Update()
+    {
+        transform.RotateAround(center.position, axis, rotationSpeed * Time.deltaTime);
+        var desiredPosition = (transform.position - center.position).normalized * radius + center.position;
+        transform.position = Vector3.MoveTowards(transform.position, desiredPosition, Time.deltaTime * radiusSpeed);
+    }
 }
