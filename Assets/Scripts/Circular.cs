d29c3372e4676e965fe230edb4d9fc9a079b2e77
wiki0831 <wiki0831@gmail.com>
2019-03-05 21:48:30 -0500

    Circular drive update

--- Assets/Scripts/Circular.cs
+++ Assets/Scripts/Circular.cs
@@ -1,24 +1,20 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
-public class Circular : MonoBehaviour {
+public class Circular : MonoBehaviour
+{
     public Transform center;
-    public Vector3 axis = Vector3.up;
-    public float radius = 2.0F;
-    public float radiusSpeed = 0.5F;
-    public float rotationSpeed = 80.0F;
+    public Vector3 axis = Vector3.right;
+    public Vector3 moveTo;
 
-    void Start()
-    {
-        center = gameObject.transform;
-        transform.position = (transform.position - center.position).normalized * radius + center.position;
-    }
+    public float radius = 2.0f;
+    public float radiusSpeed = 0.5f;
+    public float rotationSpeed = 80.0f;
 
     void Update()
     {
         transform.RotateAround(center.position, axis, rotationSpeed * Time.deltaTime);
-        var desiredPosition = (transform.position - center.position).normalized * radius + center.position;
-        transform.position = Vector3.MoveTowards(transform.position, desiredPosition, Time.deltaTime * radiusSpeed);
+        moveTo = (transform.position - center.position).normalized * radius + center.position;
+        transform.position = Vector3.MoveTowards(transform.position, moveTo, Time.deltaTime * radiusSpeed);
     }
 }
