5773dd01be215d9ae41ce7cf46f1095d3c789d9a
wiki0831 <wiki0831@gmail.com>
2019-03-21 02:12:02 -0400

    Configure joint between soundtracks

---
+++ TempoSYNTH-Unity/Assets/Scripts/Motion/SplitOnTrackContact.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplitOnTrackContact : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Track")
+        {
+            Debug.Log(other.name + "  Need to split");
+            Debug.Log(other.gameObject.GetComponent<Collider>().ClosestPointOnBounds(transform.position));
+        }
+    }
+}
